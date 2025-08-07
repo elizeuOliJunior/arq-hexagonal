b0039b9e7130bfe1ba531cb3ad172c9b9944ff1e
elizeuOliJunior <elizeugojr@gmail.com>
2025-08-07 10:30:53 -0300

    feat:first commit repository

---
+++ src/test/java/com/elizgoj/hexagonal/hexagonalarchitecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elizgoj.hexagonal.hexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
